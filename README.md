b9ab1e7000b0d12d96600b232018f187bcd21e59
tibotiber <thibaut@smplrspace.com>
2023-03-13 14:16:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# examples-hello-world-ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/smplr-hello-worls-ts)
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { loadSmplrJs, Smplr } from '@smplrspace/smplr-loader';
+
+export function initSpace(smplr: Smplr) {
+  const space = new smplr.Space({
+    spaceId: 'edb2ebaa-47ea-4e54-af0d-cf543328bdb0',
+    clientToken: 'pub_eb760fee77634cdab2fe31146fc371c2',
+    containerId: 'test',
+  });
+  space.startViewer({
+    preview: true,
+    onReady: () => console.log('Viewer is ready'),
+    onError: (error) => console.error('Could not start viewer', error),
+  });
+}
+
+loadSmplrJs('umd')
+  .then((smplr) => initSpace(smplr))
+  .catch((error) => console.error(error));
